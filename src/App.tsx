01ddc2137adc3327bd2ff1c051e11f2106343ae7
mellamadrama <s10258027@connect.np.edu.sg>
2024-10-24 10:27:16 +0800

    created the front end for the sign up booking page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import OCBCsignup from './OCBCsignup.tsx'
+import BookingPage from './BookingPage.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +11,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <OCBCsignup />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <BookingPage />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
