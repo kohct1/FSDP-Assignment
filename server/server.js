39f5d1549caeef8e9c4b0c321445a8b51eb211bd
kohct1 <kohct2006@gmail.com>
2024-10-25 14:37:06 +0800

    feat: add login functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import login from "./routes/login.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/", login);
 
-// start the Express server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
