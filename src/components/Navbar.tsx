06b286b87f48e4f5cc47a2af71bfb4b1e80ad7f5
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2024-11-12 23:47:48 +0800

    fix: removed duplicate navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,14 +87,7 @@ function Navbar() {
     return (
         <div className="relative z-20 w-full border-b border-gray-300 p-4 bg-white bg-opacity-100 pl-20 pr-20">
                 <div className="flex justify-between items-center w-full">
-                    <img
-                        src='/images/OCBC-Bank-Logo.png' // OCBC logo link
-                        alt="OCBC Logo"
-                        className="h-12"
-                    />
-                    <PopulateLinks
-                        role = {decodedRole}
-                    />
+                  
                     <Link to="/homepage">
                         <img
                             src='/images/OCBC-Bank-Logo.png' // OCBC logo link
@@ -103,9 +96,9 @@ function Navbar() {
                         />
                     </Link>
                     <div className="flex space-x-10">
-                        <Link to="/bookingpage" className="text-gray-600 font-semibold">Booking</Link>
-                        <Link to="/ticketing" className="text-gray-600 font-semibold">Queue</Link>
-                        <Link to="/user/enquiries/view" className="text-gray-600 font-semibold">Enquiry Portal</Link>
+                    <PopulateLinks
+                        role = {decodedRole}
+                    />
                         {userId ? (
                             <button 
                                 onClick={handleShowLogoutPrompt}
