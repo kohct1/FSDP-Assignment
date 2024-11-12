6080a633f1bcafbf1395db04c8ccb27e6469e01b
kohct1 <kohct2006@gmail.com>
2024-11-12 15:40:55 +0800

    feat: add booking date page ux improvements

--- src/BookingDate.tsx
+++ src/BookingDate.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import Navbar from "./components/Navbar";
@@ -10,6 +10,9 @@ function BookingDate() {
     const [bookings, setBookings] = useState<any>({});
     const [hasBookings, setHasBookings] = useState<boolean>(false);
     const [userBookings, setUserBookings] = useState<any>({});
+    const [selectedTime, setSelectedTime] = useState<number>(10);
+    const [selectedSlot, setSelectedSlot] = useState<number[]>([]);
+    const [displayTime, setDisplayTime] = useState<string[]>([]);
     const { state } = useLocation();
     const navigate = useNavigate();
     const currentDate: Date = new Date();
@@ -97,6 +100,11 @@ function BookingDate() {
         }
     }
 
+    function setSelectedTimeAndSlot(time: number, slot: number[]): void {
+        setSelectedTime(time);
+        setSelectedSlot(slot);
+    }
+
     useEffect(() => {
         getUser();
     }, []);
@@ -108,6 +116,29 @@ function BookingDate() {
     useEffect(() => {
         getBookings();
     }, [selectedDate]);
+
+    useEffect(() => {
+        let time1: number = selectedTime;
+        let time2: number = selectedTime;
+        let slot1: string = String((selectedSlot[0] - 1) * 10);
+        let slot2: string = String((selectedSlot[1]) * 10);
+
+        if (slot1 === "0" || slot1 === "60") slot1 = "00";
+        if (slot2 === "0" || slot2 === "60") {
+            slot2 = "00";
+            
+            if (time2 === 12) {
+                time2 = 1;
+            } else {
+                time2 += 1;
+            }
+        }
+
+        let displayTime1: string = `${time1}.${slot1}`;
+        let displayTime2: string = `${time2}.${slot2}`;
+
+        setDisplayTime([displayTime1, displayTime2]);
+    }, [selectedSlot]);
 
     return (
         <div className="w-full h-screen flex flex-col items-center">
@@ -122,13 +153,13 @@ function BookingDate() {
                                 if (index !== 0 && index % 7 === 0) {
                                     return (
                                         <div key={index} className="w-full flex gap-8">
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 6)}>{index - 6}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 5)}>{index - 5}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 4)}>{index - 4}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 3)}>{index - 3}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 2)}>{index - 2}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 1)}>{index - 1}</div>
-                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index)}>{index}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 6)}>{index - 6 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 6}</div> : index - 6}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 5)}>{index - 5 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 5}</div> : index - 5}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 4)}>{index - 4 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 4}</div> : index - 4}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 3)}>{index - 3 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 3}</div> : index - 3}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 2)}>{index - 2 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 2}</div> : index - 2}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 1)}>{index - 1 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index - 1}</div> : index - 1}</div>
+                                            <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index)}>{index === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index}</div> : index}</div>
                                         </div>
                                     );
                                 }
@@ -137,8 +168,8 @@ function BookingDate() {
                                     if (index + 1 === 30) {
                                         return (
                                             <div key={index} className="w-full flex gap-8">
-                                                <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index)}>{index}</div>
-                                                <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index - 1)}>{index + 1}</div>
+                                                <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index)}>{index === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index}</div> : index}</div>
+                                                <div className="w-12 h-12 flex justify-center items-center rounded hover:bg-slate-200" onClick={() => setSelectedDate(index + 1)}>{index + 1 === selectedDate ? <div className="w-full h-full flex justify-center items-center bg-slate-100 rounded">{index + 1}</div> : index + 1}</div>
                                             </div>
                                         );
                                     }
@@ -154,7 +185,11 @@ function BookingDate() {
                             })}
                         </div>  
                     </div>
+                    <div className="w-full flex flex-col items-center gap-2">
+                        <h1>{selectedSlot.length === 0 ? "Select a booking date and time" : `Book for: ${months[currentDate.getMonth()]} ${selectedDate} ${String(currentDate.getFullYear())}, ${displayTime[0]} - ${displayTime[1]}`}</h1>
+                        <motion.button className="w-1/4 bg-red-600 rounded text-white font-semibold m-4 px-3 py-2" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} onClick={() => createUserBooking(selectedTime, selectedSlot)}>Confirm</motion.button>
                     </div>
+                </div>
                 <div className="w-2/5 h-full bg-slate-100 overflow-scroll">
                     <div className="w-full h-1/6 flex flex-col justify-center items-start px-8">
                         <h1 className="text-2xl font-semibold">Available time slots for:</h1>
@@ -210,10 +245,15 @@ function BookingDate() {
                                                 }
                                             }
 
-
                                             if (slots.includes(slot + suggestedSlots - 1)) {
+                                                if (time === selectedTime && selectedSlot[0] === slot && selectedSlot[1] === slot + suggestedSlots - 1) {
+                                                    return (
+                                                        <motion.button key={index} className="w-1/4 bg-red-700 rounded text-white font-semibold m-4 px-3 py-2" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>{`${time1}.${slot1} - ${time2}.${slot2}`}</motion.button>
+                                                    );
+                                                }
+
                                                 return (
-                                                    <motion.button key={index} className="w-1/4 bg-red-600 rounded text-white font-semibold m-4 px-3 py-2" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} onClick={() => createUserBooking(time, [slot, slot + suggestedSlots - 1])}>{`${time1}.${slot1} - ${time2}.${slot2}`}</motion.button>
+                                                    <motion.button key={index} className="w-1/4 bg-red-600 rounded text-white font-semibold m-4 px-3 py-2" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} onClick={() => setSelectedTimeAndSlot(time, [slot, slot + suggestedSlots - 1])}>{`${time1}.${slot1} - ${time2}.${slot2}`}</motion.button>
                                                 );
                                             }
                                         })}
