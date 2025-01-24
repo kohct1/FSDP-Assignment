35c5e977918090773788c16a5e484fa6b68a44ac
sudarsanamrithika <veethe2nd@gmail.com>
2025-01-24 22:08:42 +0800

    merge staff queue

--- src/StaffQueue.tsx
+++ src/StaffQueue.tsx
@@ -63,7 +63,6 @@ const StaffQueue = () => {
             setCurrentCall(leftQueue + 1); // Set the currently serving call
             setCallStarted(true); // Indicate that a call has started
             setLastUpdatedTime(getCurrentTime());
-            navigate("/webcall");
         } else {
             alert("No one in the queue to call!");
         }
