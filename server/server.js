4c939fd3b0b9aada543a0523d9a6bde1af27fe5e
weiyingeve <s10257093@connect.np.edu.sg>
2025-02-01 14:18:42 +0800

    fixed delete booking issue but google calendar event popup not working

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5050;
 
 app.use(cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT"], 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type"],
 }));
 app.use(express.json());
